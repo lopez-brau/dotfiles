9f6377ad476e57197cafc580096431ed0bc519e9
lopez-brau <michael.lopez.brau@gmail.com>
2025-08-16 14:17:54 -0400

    Updated TypeScript LSP server name.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "tsserver" }
+        ensure_installed = { "lua_ls", "pyright", "ts_ls" }
       })
     end
   },
@@ -27,7 +27,7 @@ return {
         }
       })
       lspconfig.pyright.setup({})
-      lspconfig.tsserver.setup({})
+      lspconfig.ts_ls.setup({})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})
