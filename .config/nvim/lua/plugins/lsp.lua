68a268c4551f780f833af21092f75105ce84b38a
lopez-brau <michael.lopez.brau@gmail.com>
2025-11-01 12:26:19 -0400

    Rearranged plugin order, updated `nvim-lspconfig` setup according to new spec, and added "clangd" LSP.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,18 +6,10 @@ return {
     end
   },
   {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "ts_ls" }
-      })
-    end
-  },
-  {
     "neovim/nvim-lspconfig",
     config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
+      -- Get the language server to recognize the `vim` global.
+      vim.lsp.config("lua_ls", {
         settings = {
           Lua = {
             diagnostics = {
@@ -26,11 +18,17 @@ return {
           }
         }
       })
-      lspconfig.pyright.setup({})
-      lspconfig.ts_ls.setup({})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "clangd", "lua_ls", "pyright", "ts_ls" }
+      })
     end
   }
 }
